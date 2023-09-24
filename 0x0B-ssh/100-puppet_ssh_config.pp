60147900b15efd03d3d1781df1a8d5400e7f1e08
jailan-sh <jilan.elsherbiny@gmail.com>
2023-09-24 15:39:31 -0700

    :(

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # set up your client SSH configuration file so that you can connect to a server without typing a password.
-file_line{'identity file':
-path   => '/etc/ssh/ssh_config'
-match  => '^ IdentityFile '
-line   => ' IdentityFile ~/.ssh/school'
+file_line{'set identity file':
+	path   => '/etc/ssh/ssh_config',
+	match  => '^ IdentityFile ',
+	line   => ' IdentityFile ~/.ssh/school'
 }
 file_line{'passwd auth':
-path   => '/etc/ssh/ssh_config'
-match  => '^ PasswordAuthentication '
-line   => 'no'
+	path   => '/etc/ssh/ssh_config',
+	match  => '^ PasswordAuthentication ',
+	line   => 'PasswordAuthentication no'
 }
