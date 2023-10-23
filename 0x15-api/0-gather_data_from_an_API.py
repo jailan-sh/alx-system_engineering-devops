6360bdf34cc21f478459ed23367cb43d0ee348ea
jailan-sh <jilan.elsherbiny@gmail.com>
2023-10-24 01:42:35 +0300

    pycode

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,30 +2,31 @@
 """ script to for returning information about TODO list progress
 of a given employee ID
 """
+
 import json
 import requests
 import sys
 
 
-url1 = "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
-url2 = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
-    sys.argv[1])
+if __name__ = "__main__":
+    url1 = "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
+    url2 = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+        sys.argv[1])
 
-resp1 = requests.get(url1)
-resp2 = requests.get(url2)
+    resp1 = requests.get(url1)
+    resp2 = requests.get(url2)
 
-name = resp1.json()["name"]
-data = resp2.json()
-total = len(data)
-done = 0
-title = []
+    name = resp1.json()["name"]
+    data = resp2.json()
+    total = len(data)
+    done = 0
+    title = []
 
-for item in data:
-    if item["completed"]:
-        done += 1
-        title.append(item["title"])
+    for item in data:
+        if item["completed"]:
+            done += 1
+            title.append(item["title"])
 
-
-print("Employee {} is done with tasks({}/{}):".format(name, done, total))
-for item in title:
-    print("\t{}".format(item))
+    print("Employee {} is done with tasks({}/{}):".format(name, done, total))
+    for item in title:
+        print("\t{}".format(item))
