0e2f4286326df2c15487e248a1384f620b5e4862
jailan-sh <jilan.elsherbiny@gmail.com>
2023-10-24 22:20:02 +0300

    4

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,22 +9,21 @@ if __name__ == "__main__":
     import sys
 
     url1 = "https://jsonplaceholder.typicode.com/users"
-    url2 = "https://jsonplaceholder.typicode.com/todos"
 
     resp1 = requests.get(url1)
-    resp2 = requests.get(url2)
+    
+    names = resp1.json()
+    items = {}
+    all = []
+    for user in names:
+        url2 = "https://jsonplaceholder.typicode.com/users/{}/todos".format(names["id"])
+        
+        resp2 = requests.get(url2)
+        data = repo2.json()
+        for task in data:
+            items = {"username": names["username"], "task": task["title"], "completed": task["completed"]}
+        all .append(items)
+        inf = {names["id"]: all}
 
-    name = resp1.json()
-    data = resp2.json()
-
-    tasks = []
-    item = {}
-
-    for element in data:
-        item = {"task": element["title"], "completed": element["completed"],
-                "username": name["username"]}
-        tasks.append(item)
-
-    inf = {name["id"]: tasks}
     with open("todo_all_employees.json", mode="w") as file:
         json.dump(inf, file)
