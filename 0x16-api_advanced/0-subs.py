cc5a90d94ce4b83002e99b0b7b35c61a2206814d
jailan-sh <jilan.elsherbiny@gmail.com>
2023-11-08 01:04:15 +0200

    add

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,3 +2,21 @@
 """
 a function that queries the Reddit API and returns the number of subscribers
 """
+
+import requests
+import json
+
+
+def number_of_subscribers(subreddit):
+    """function to get subscribers """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'user-agent': 'jailan'}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
