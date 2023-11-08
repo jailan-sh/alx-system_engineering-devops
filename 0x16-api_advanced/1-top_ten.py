67071b7ab98977a420b52d8881b51d746104c0d8
jailan-sh <jilan.elsherbiny@gmail.com>
2023-11-08 02:19:14 +0200

    add

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,7 +12,7 @@ def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts listed for a subreddit"""
     headers = {"user-agent": "jailan"}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print("None")
         return
