6ae15fa536ec0f13ce315c17523d06420a1012ad
jailan-sh <jilan.elsherbiny@gmail.com>
2023-11-08 19:39:07 +0200

    add

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,20 +7,20 @@ for a given subreddit
 import requests
 import json
 
+
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts listed for a subreddit"""
-    headers = {"User-Agent": "jailan"}  # Changed user-agent header
-    params = {"limit": 10}
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    response = requests.get(url, headers=headers, params=params)
-
-    if response.status_code != 200:
-        print("Error:", response.status_code)
-        return
-    data = response.json().get('data', {}).get('children', [])
-
-    if not data:
-        print("No posts found.")
-        return
-    for item in data:
-        print(item.get('data', {}).get('title'))
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'jailan'}
+    params = {"limit": 10, }
+    response = requests.get(url, headers=headers, allow_redirects=False,
+                            params=params)
+    if response.status_code == 200:
+        data = response.json()
+        if 'data' in data and 'children' in data['data']:
+            for post in data['data']['children']:
+                print(post['data']['title'])
+        else:
+            print("None")
+    else:
+        print("None")
