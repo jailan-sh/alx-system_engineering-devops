5ebab73a933341492941aefd5ea30bccd0e214d3
jailan-sh <jilan.elsherbiny@gmail.com>
2023-11-08 02:00:23 +0200

    add

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Querries the Reddit API and prints the titles of the first 10 hot posts listed
+for a given subreddit
+"""
+
+import requests
+import json
+
+
+def top_ten(subreddit):
+    """Prints the titles of the first 10 hot posts listed for a subreddit"""
+    headers = {"user-agent": "jailan"}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        print("None")
+        return
+    data = response.json().get('data').get('children')
+    if not data:
+        print("None")
+        return
+    for item in data[0:10]:
+        print(item.get('data').get('title'))
