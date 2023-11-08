8eb018ceaf8695f0352694a6e9a98568e39ac600
jailan-sh <jilan.elsherbiny@gmail.com>
2023-11-08 02:06:11 +0200

    add

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Querries the Reddit API and returns a list of the titles of all hot posts
+listed for a given subreddit
+"""
+
+import json
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """Return a list of the titles of all hot posts listed for a subreddit"""
+    headers = {"user-agent": "holberton"}
+    params = {"after": after}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code != 200:
+        return None
+    data = json.loads(response.text).get('data').get('children')
+    after = json.loads(response.text).get('data').get('after')
+    if data is None:
+        if len(hot_list) == 0:
+            return None
+        return hot_list
+    else:
+        for item in data:
+            hot_list.append(item.get('data').get('title'))
+    if after is None:
+        if len(hot_list) == 0:
+            return None
+        return hot_list
+    else:
+        return recurse(subreddit, hot_list, after)
