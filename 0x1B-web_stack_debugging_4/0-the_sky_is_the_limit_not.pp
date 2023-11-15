8b240e29613160c2cd6e4d7f710ef357272c0242
jailan-sh <jilan.elsherbiny@gmail.com>
2023-11-15 21:01:11 +0200

    restart nginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,4 +2,10 @@
 file { '/etc/default/nginx':
   ensure  => present,
   content => "ULIMIT='-n 4096'\n",
+  notify  => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure => running,
+  enable => true,
 }
