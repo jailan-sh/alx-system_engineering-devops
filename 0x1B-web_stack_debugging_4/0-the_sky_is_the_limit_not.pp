17907f3779fce779377b446300a6fdd3af39adab
jailan-sh <jilan.elsherbiny@gmail.com>
2023-11-15 20:54:32 +0200

    run

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # fix error by fix defult number of requests allowed in the web server 4096
 file { '/etc/default/nginx':
   ensure  => present,
-  content => "ULIMIT='-n 15'\n",
+  content => "ULIMIT='-n 4096'\n",
 }
